015a66bbfbb78e055cd67293cc8802233b506956
ajohnson <ajohnson@pop-os.home.local>
2025-03-17 20:22:17 -0700

    small pieces of a navbar in vue

---
+++ vue-ideas/frontend-snips/src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from "vue"
+
+// here we'll put the most common routes that could be used to check out weird app ideas.
+
+const navList = ["Slider 1", "Confetti Spray"]
+
+</script>
+
+<template>
+<ul>
+    <li v-for="navItem in navList">
+        {{navItem}}
+    </li>
+</ul>
+
+
+</template>
+
+<style>
+
+</style>
